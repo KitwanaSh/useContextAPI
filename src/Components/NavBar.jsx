f84c64ea843f198dafedbeb1e4367c87a213dfbf
KitwanaSh <rodriguekitweze@gmail.com>
2024-02-17 18:37:55 +0000

    Feat: Add multiple consumer

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
+import { AuthContext } from "../contexts/AuthContext";
 
 class NavBar extends Component {
   render() {
     return (
-      <ThemeContext.Consumer>
-        {(context) => {
-          const { isLightTheme, light, dark } = context;
+      <AuthContext.Consumer>{(contextAuth) => (
+        <ThemeContext.Consumer>
+        {(contextTheme) => {
+          const { isAuthenticated, toggleAuth } = contextAuth;
+          const { isLightTheme, light, dark } = contextTheme;
           const theme = isLightTheme ? light : dark;
           return (
             <nav style={{ background: theme.ui, color: theme.syntax }}>
               <h1>Context App</h1>
+              <div onClick={toggleAuth} style={{ cursor: "pointer"}}>
+                {isAuthenticated ? "Log In": "Log Out"}
+              </div>
               <ul>
                 <li>Home</li>
                 <li>About</li>
@@ -20,6 +26,8 @@ class NavBar extends Component {
           );
         }}
       </ThemeContext.Consumer>
+      )}
+      </AuthContext.Consumer>  
     );
   }
 }
